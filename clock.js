313357e3b558779573b0d1df75485c67a16e6735
Matt Wilkerson <mattisonwilkerson@gmail.com>
2015-02-09 20:19:26 -0500

    JS clock in black

---
+++ clock.js
@@ -0,0 +1,29 @@
+
+
+var counter = function() {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  
+  if (hours.toString().length < 2) {
+  hours = "0" + hours;
+}
+  if (minutes.toString().length < 2) {
+  minutes = "0" + minutes;
+}
+  if (seconds.toString().length < 2) {
+  seconds = "0" + seconds;
+}
+ 
+  $("span").text(hours + ":" + minutes + ":" + seconds);
+  
+}
+
+$(function(){
+  
+  counter();
+  
+  var intID = setInterval(counter, 1000);
+  
+});
